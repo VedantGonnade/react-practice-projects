7acc1fd7543f0216861f88310a0d2fdd55e9a973
Vedant Gonnade <vedantgonnade@outlook.com>
2025-02-01 21:28:58 +0100

    installed extensions

---
+++ tutorial/src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDom from "react-dom/client";
+
+export const Greeting = () => {
+  return (
+    <h2 id="id" className="class">
+      My React
+    </h2>
+  );
+};
+
+const root = ReactDom.createRoot(document.getElementById("root"));
+root.render(<Greeting />);
