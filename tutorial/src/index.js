7d7a28baa0f0376819fb5c59c1584beaec195e46
Vedant Gonnade <vedantgonnade@outlook.com>
2025-02-02 21:59:25 +0100

    used props in the component and special prop called children

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -3,37 +3,48 @@ import ReactDom from "react-dom/client";
 
 import "./index.css";
 
+const firstBook = {
+  img: "https://m.media-amazon.com/images/I/71FsIkGF3pL._AC_UY218_.jpg",
+  title: "The Midnight Library",
+  author: "Matt Haig",
+};
+
+const secondBook = {
+  img: "https://m.media-amazon.com/images/I/61aNIVRCtHL._AC_UY218_.jpg",
+  title: "Atomic habits",
+  author: "James clear",
+};
+
 export const BookList = () => {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book
+        author={firstBook.author}
+        title={firstBook.title}
+        img={firstBook.img}
+      >
+        <button>I am a children</button>
+      </Book>
+      <Book
+        author={secondBook.author}
+        title={secondBook.title}
+        img={secondBook.img}
+      />
     </section>
   );
 };
 
-const author = "Matt Haig";
-
-const Book = () => {
-  const title = "The Midnight Library";
+const Book = props => {
+  const { img, title, author, children } = props;
   return (
     <article className="book">
-      <img
-        src="https://m.media-amazon.com/images/I/71FsIkGF3pL._AC_UY218_.jpg"
-        alt=""
-      />
+      <img src={img} alt={title} />
       <h2>{title}</h2>
-      <h4
-        style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.5rem" }}
-      >
-        {author}
-      </h4>
+      <h4>{author}</h4>
+      {children}
     </article>
   );
 };
 
 const root = ReactDom.createRoot(document.getElementById("root"));
 root.render(<BookList />);
-
