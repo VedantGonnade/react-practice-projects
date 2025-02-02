3698609d75b1fe7d790d6f4838b4aebe90494e74
Vedant Gonnade <vedantgonnade@outlook.com>
2025-02-02 14:21:39 +0100

    tried css in react

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,15 +1,39 @@
 import React from "react";
 import ReactDom from "react-dom/client";
 
-export const Greeting = () => {
+import "./index.css";
+
+export const BookList = () => {
   return (
-    <>
-      <h2 id="id" className="class">
-        My React
-      </h2>
-    </>
+    <section className="booklist">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
+};
+
+const author = "Matt Haig";
+
+const Book = () => {
+  const title = "The Midnight Library";
+  return (
+    <article className="book">
+      <img
+        src="https://m.media-amazon.com/images/I/71FsIkGF3pL._AC_UY218_.jpg"
+        alt=""
+      />
+      <h2>{title}</h2>
+      <h4
+        style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.5rem" }}
+      >
+        {author}
+      </h4>
+    </article>
   );
 };
 
 const root = ReactDom.createRoot(document.getElementById("root"));
-root.render(<Greeting />);
+root.render(<BookList />);
+
